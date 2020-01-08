e9475354a6dbaceb0452c986d17f6ab02a2ee617
ChaosAlphard <Alphard450@outlook.com>
2020-01-08 16:16:37 +0800

    更新获取小程序获取订阅消息功能

--- source/WX/Controllers/WXApiController.cs
+++ source/WX/Controllers/WXApiController.cs
@@ -8,19 +8,37 @@ using WX.Cache;
 using WX.Dto;
 using WX.Model;
 using WX.Service;
+using WX.Util;
 
 namespace WX.Controllers {
     [ApiController]
     [Route("wx")]
     public class WXApiController : ControllerBase {
-        private AccessTokenCache cache = new AccessTokenCache();
+        private WXApiService wx = new WXApiImpl();
 
+        /**
+         * @param: msg 消息
+         * @param: from 发送者的openid
+         */
         [HttpGet("applyForRepair")]
-        public VDto<User> applyForRepair(string code) {
-            if(isInvalid(code)) {
-                return VDto<User>.Of(Status.LOST_PARAM);
+        public VDto<SubMsgResult> applyForRepair(string msg, string from) {
+            if(isInvalid(msg)&&isInvalid(from)) {
+                return VDto<SubMsgResult>.Of(Status.LOST_PARAM);
             }
-            throw new NotImplementedException();
+            // 管理者的openid
+            string managerOpenid = "oqFTn5Yr2QNr_-492HBzh7I_w53Y";
+            // 消息模板的templateid
+            string templateid = "8yJvg3O8n7FReHC8KUDChlFMoxNIDqEAvzGr2o_Vx5o";
+            // 点击跳转的page路径
+            string page = $"pages/manage/manage?openid={from}&msg={msg}";
+            // 通知消息的Entity
+            RemindMsg message = new RemindMsg();
+            message.thing1 = Val.Of(msg);
+            message.date2 = Val.Of(DateUtil.getCurrentTimeStr());
+            // 返回结果
+            var response = wx.requireSubMsg<SubMsgResult, RemindMsg>(managerOpenid, templateid, message, page);
+
+            return VDto<SubMsgResult>.OfModel(Status.GET_DATA_SUCCESS, response);
         }
 
         [HttpGet("replyRepair")]
